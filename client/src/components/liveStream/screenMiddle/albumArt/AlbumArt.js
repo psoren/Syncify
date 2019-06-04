cb1900a95a228f73750163282de6da8ec184d661
Parker <p.sorenson@me.com>
2019-06-03 21:45:49 -0600

    room now shows when it is paused

--- client/src/components/liveStream/screenMiddle/albumArt/AlbumArt.js
+++ client/src/components/liveStream/screenMiddle/albumArt/AlbumArt.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import { LiveStreamContext } from '../../LiveStream';
 import './albums.scss';
 
-export default (props) => (
-	<div className='albumsOuter'>
-		<img className='side' src={props.albumArt.albumLeft ?
-			props.albumArt.albumLeft : '../notPlaying.jpg'}
-			alt='Left Album' />
-		<img className='middle'
-			src={props.albumArt.albumMiddle ?
-				props.albumArt.albumMiddle : '../notPlaying.jpg'}
-			alt='Middle Album' />
-		<img className='side' src={props.albumArt.albumRight ?
-			props.albumArt.albumRight : '../notPlaying.jpg'}
-			alt='Right Album' />
-	</div>
-);
+class AlbumArt extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = { isPlaying: true };
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.isPlaying !== this.state.isPlaying) {
+			this.setState({ isPlaying: !this.state.isPlaying });
+		}
+	}
+
+	render() {
+		return (
+			<div className='albumsOuter'>
+				<img className='side' src={this.props.albumArt.albumLeft ?
+					this.props.albumArt.albumLeft : '../notPlaying.jpg'}
+					alt='Left Album'
+				/>
+				<div className='middleContainer'>
+					<img className='middleImg'
+						src={this.props.albumArt.albumMiddle ?
+							this.props.albumArt.albumMiddle : '../notPlaying.jpg'}
+						alt='Middle Album'
+					/>
+					{this.state.isPlaying ?
+						<img className='middlePaused'
+							src='liveStreamPause.svg'
+							alt='Paused'
+						/> : null}
+					{this.state.isPlaying ?
+						<div className='middleGrayBackground'
+						/> : null}
+				</div>
+				<img className='side' src={this.props.albumArt.albumRight ?
+					this.props.albumArt.albumRight : '../notPlaying.jpg'}
+					alt='Right Album'
+				/>
+			</div>
+		);
+	}
+
+}
+
+export default props => (<LiveStreamContext.Consumer>
+	{context => { return <AlbumArt {...props} context={context} /> }}
+</LiveStreamContext.Consumer>)
